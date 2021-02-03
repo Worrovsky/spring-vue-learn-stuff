36745e0b626def4b4dcc63dcd3121c657ea61565
pnzhiltsov@gmail.com <pnzhiltsov@gmail.com>
2021-02-03 17:00:27 +0300

    p001: begin work on front

---
+++ 001-rest-h2-vue/frontend/src/components/Products.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-list>
+        <v-list-item v-for="(p, i) in products" :key="i">
+          <v-list-item-content>
+            <v-list-item-content>
+              {{ p.name }}
+            </v-list-item-content>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-btn icon>
+              <v-icon color="grey lighten-1">mdi-information</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Products',
+
+  data: () => ({
+    products: [{ name: 'Milk' }, { name: 'Tea' }],
+  }),
+}
+</script>
