818de819763a111bbbeeb8988f368a8993bc6ac9
pnzhiltsov@gmail.com <pnzhiltsov@gmail.com>
2021-05-26 23:18:16 +0300

    p005 init commit

---
+++ 005-security-thymeleaf/README.md
@@ -0,0 +1,3 @@
+WIP
+
+https://github.com/eugenp/learn-spring-security/tree/module2
---
+++ 005-security-thymeleaf/src/main/java/com/example/web/RegistrationController.java
@@ -0,0 +1,15 @@
+package com.example.web;
+
+import com.example.web.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("/signup")
+    public ModelAndView signup() {
+        return new ModelAndView("registrationPage", "user", new User());
+    }
+}
