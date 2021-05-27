50519d8037497bba3d15ccd3d251f2ca5dd6f528
pnzhiltsov@gmail.com <pnzhiltsov@gmail.com>
2021-05-27 23:59:59 +0300

    WIP

--- 005-security-thymeleaf/README.md
+++ 005-security-thymeleaf/README.md
@@ -1,3 +1,5 @@
 WIP
 
 https://github.com/eugenp/learn-spring-security/tree/module2
+
+[про DevTools](https://habr.com/ru/post/479382/)
--- 005-security-thymeleaf/src/main/java/com/example/web/RegistrationController.java
+++ 005-security-thymeleaf/src/main/java/com/example/web/RegistrationController.java
@@ -3,6 +3,7 @@ package com.example.web;
 import com.example.web.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,5 +12,10 @@ public class RegistrationController {
     @GetMapping("/signup")
     public ModelAndView signup() {
         return new ModelAndView("registrationPage", "user", new User());
+    }
+
+    @PostMapping("/user/register")
+    public ModelAndView registerUser() {
+        return new ModelAndView("redirect:/login");
     }
 }
