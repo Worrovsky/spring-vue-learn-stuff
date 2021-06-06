3174c066dcbd0d085b05f2852b6e3598ace77094
Worrovsky <Worrovsky@users.noreply.github.com>
2021-06-07 00:28:04 +0300

    p005 [WIP] add user details service

---
+++ 005-security-thymeleaf/src/main/java/com/example/security/UserDetailsServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.security;
+
+import com.example.persistent.UserRepository;
+import com.example.registration.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private static final String ROLE_USER = "ROLE_USER";
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user with " + email);
+        }
+
+        UserDetails details = new org.springframework.security.core.userdetails.User(
+                user.getEmail()
+                , user.getPassword()
+                , true
+                , true
+                , true
+                , true
+                , getAuthorities(ROLE_USER));
+        return details;
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return Arrays.asList(new SimpleGrantedAuthority(role));
+    }
+}
